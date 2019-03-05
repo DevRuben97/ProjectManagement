b28a457ed08c886fb03d1f229e7f128c6373dabb
Ruben A. Batista <alexbatista158@hotmail.com>
2019-03-05 17:12:13 -0400

    Diseño de la pagina principal de proyectos
    Se ha resuelto el problema de coneccion con firebase, diseñodo la na-
    vegacion principal, el boton de agregar proyectos y el listado del mi-
    smo.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const config= {
+    apiKey: "AIzaSyCYvqT2IjwWfT7lbIG1KPkH2_YCYUT6AOw",
+    authDomain: "projectmanager-bac26.firebaseapp.com",
+    databaseURL: "https://projectmanager-bac26.firebaseio.com",
+    projectId: "projectmanager-bac26",
+    storageBucket: "projectmanager-bac26.appspot.com",
+    messagingSenderId: "755466272035"
+};
+var database;
+$(document).ready(function(){
+
+     // Initialize Firebase
+     firebase.initializeApp(config);
+     //DataBase reference:
+     database= firebase.database();
+})
