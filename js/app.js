214be8da8ba3d1b88104cd05f7b3de241f3d0c47
Ruben A. Batista <alexbatista158@hotmail.com>
2019-03-15 18:42:45 -0400

    Diseño de las paginas restantes

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ const config= {
     messagingSenderId: "755466272035"
 };
 var database;
+var btnNewButton;
+var btnCloseButton;
 //Global variables:
 $(document).ready(function(){ //Initial Config
 
@@ -16,7 +18,22 @@ $(document).ready(function(){ //Initial Config
      //DataBase reference:
      database= firebase.database();
 
-     document.getElementById("btnSaveProject").onclick= SaveNewProject;
+
+     //Determinar el tipo de configuracion:
+     var ruta= window.location.href;
+     if (ruta.endsWith("/AdminPanel.html")){
+        document.getElementById("btnSaveProject").onclick= SaveNewProject;
+     }
+     else if (ruta.endsWith("/Home.html")){
+         document.getElementById("")
+     }
+     else if (ruta.endsWith("/ProjectTask.html")){
+
+     }
+     
+
+     
+     
 })
 
 //#region Projects Functions
@@ -31,5 +48,61 @@ function SaveNewProject(){
 }
 function Get_ProjectList(){
 
+}
+//#endregion
+
+//#region Users
+function AddUser(){
+
+    $("#RepeatPassDiv").attr("hidden",false);
+    var Savebutton= $("#btnSaveProject");
+    Savebutton.off("click");
+    Savebutton.on("click",function(){
+
+        //Save the new user:
+        var email= $("#txtCorreo");
+        var password= $("#txtContraseña");
+        var RepPassword= $("#txtRepeatPassword");
+        //Validate the data:
+        var data= [email.val(), password.val(), RepPassword.val()]
+
+        
+    })
+    
+}
+function Login(){
+
+    //Login user 
+    $("#RepeatPassDiv").attr("hidden",true);
+    var LoginButton= $("#btnSaveProject");
+    LoginButton.off("click");
+    LoginButton.on("click",function(){
+
+        
+    })
+}
+function LoginOut(){
+
+}
+function VerifyIdentity(){  //Verificar si ya se inicio la sesion en la herramienta.
+
+}
+
+//#endregion
+
+//#region Common functions
+function Validate(data){
+
+  //validate the inpusts:
+  for(e==0;e<data.length;e+=1){
+      if (data[e]== "" || data[e]== null){
+          
+      }
+  }
+
+}
+function SetError(input, message){
+    $(input).addClass("form-control is-invalid")
+    
 }
 //#endregion
